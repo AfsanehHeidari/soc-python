6f199e60034bb995c5fcc559205d39928055334b
Afsaneh.Heidari <143424548+AfsanehHeidari@users.noreply.github.com>
2025-09-27 13:15:46 +0300

    Update soc.py

--- soc.py
+++ soc.py
@@ -10,18 +10,3 @@ class Soc:
 
     def read(self, addr):
         return self.registers[addr]
-
-
-
-class Soc:
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.registers = {0x00: 0x0}
-
-    def write(self, addr, data):
-        self.registers[addr] =data
-
-    def read(self, addr):
-        return self.registers[addr]
