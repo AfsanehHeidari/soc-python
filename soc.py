4a80705322043d91e6519fc22504cae6019b524b
AfsanhHeidari <t3heaf@students.oamk.fi>
2025-09-27 13:42:30 +0300

    updated soc and test files

--- soc.py
+++ soc.py
@@ -3,25 +3,20 @@ class Soc:
         self.reset()
 
     def reset(self):
-        self.registers = {0X00: 0X0}
+        self.registers = {
+            0X00: 0X0,
+            0x04: 0x1
+       }
     
-    def write(self, addr, data):
-        self.registers[addr] = data
+    def write(self, addr: int, data: int):
+        if addr == 0x00:
+            self.registers[addr] = data
+            self.registers[0x04] = 1 if (data %2 == 0) else 0
+        else:
+            raise ValueError(f"Write to invalid  addr 0x{addr: X}")
 
-    def read(self, addr):
-        return self.registers[addr]
-
-
-
-class Soc:
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.registers = {0x00: 0x0}
-
-    def write(self, addr, data):
-        self.registers[addr] =data
-
-    def read(self, addr):
-        return self.registers[addr]
+    def read(self, addr: int) -> int:
+        if addr in self.registers:
+            return self.registers[addr]
+        else:
+            raise ValueError(f"Read fron invalid addr 0x{addr: X}")
--- test.py
+++ test.py
@@ -1,14 +1,35 @@
 from soc import Soc
+from random import randint
+
+CTRL_ADDR = 0X00
+STATUSE_ADDR = 0X04
+
 dut = Soc()
 
 print("=== Test Start ===")
 
 dut.reset()
-assert dut.read(0X00) == 0x0
+assert dut.read(0X00) == 0x0, "CTRL reset mismatch"
+assert dut.read(0x04) == 0x1, "STATUS reset mismatch"
 print("Reset passed")
 
-dut.write(0x00, 5)
-assert dut.read(0x00) == 5
-print("Write=5 -> Read=5 OK")
+dut.write(0x00, 4)
+assert dut.read(0x00) == 4, "CTRL write/read mismatch"
+assert dut.read(0x04) == 1, "STATUS side effect failed(expected even number)"
+print("CTRL=4 -> STATUS=1 OK")
 
-print("Test PASSED")
+dut.write(0x00, 5)
+assert dut.read(0x00) == 5, "CTRL write/read mismatch"
+assert dut.read(0x04) == 0, "STATUS side effect failed(expected odd number)"
+print("CTRL=5 -> STATUS=0 OK")
+
+for _ in range(10):
+    val = randint(0, 255)
+    dut.write(CTRL_ADDR, val)
+    ctrl= dut.read(CTRL_ADDR)
+    status = dut.read(STATUSE_ADDR)
+    assert ctrl == val, f"CTRL mismatch: wrote {val}, read {ctrl}"
+    assert status == (val % 2 == 0), f"STATUS mismatch for CTRL={val}"
+print("Randomized tests OK")
+
+print("Register Read/Write test PASSED")
